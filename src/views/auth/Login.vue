37ebc3e8d3732f73f2bb1fd6f23541c0fa2a59f5
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-22 01:14:30 +0200

    Create submit and import composable to work with it

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,14 +1,33 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
     <input type="email" placeholder="Email" v-model="email">
     <input type="password" placeholder="Password" v-model="password">
+    <div v-if="error" class="error">{{ error }}</div>
     <button>Log In</button>
   </form>
 </template>
 
 <script>
-export default {
+import useLogIn from '@/composables/useLogIn'
+import { ref } from '@vue/reactivity'
 
+export default {
+  setup() {
+    const { error, logIn } = useLogIn()
+
+    const email = ref('')
+    const password = ref('')
+
+    const handleSubmit = async () => {
+      const res = await logIn(email.value, password.value)
+      if (!error.value) {
+        console.log('User logged in')
+      }
+    }
+
+    return { email, password, handleSubmit, error }
+  }
 }
 </script>
 
