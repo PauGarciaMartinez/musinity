a9810b66a33793694a5ab7085e0096792a3208b2
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-26 00:40:12 +0200

    Import navbar in app general view fix it at the top of every page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="navbar">
     <nav>
-      
+      <h1><router-link :to="{ name: 'Home' }">Musinity</router-link></h1>
     </nav>
   </div>
 </template>
 
 <script>
 export default {
-
+  
 }
 </script>
 
