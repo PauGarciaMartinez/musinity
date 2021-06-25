e4757f2c9cc1ef12dc09db8455bd7fc7b6a45cfc
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-25 18:17:19 +0200

    Create pending state for database interactions

--- src/composables/useLogIn.js
+++ src/composables/useLogIn.js
@@ -1,23 +1,29 @@
+import { is } from "core-js/fn/object"
 import { ref } from "vue"
 import { projectAuth } from "../firebase/config"
 
 const error = ref(null)
+const isPending = ref(false)
 
 const logIn = async (email, password) => {
   error.value = null
+  isPending.value = true
 
   try {
     const res = await projectAuth.signInWithEmailAndPassword(email, password)
     error.value = null
+    isPending.value = false
+    return res
     
   } catch(err) {
     console.log(err.value)
     error.value = 'Incorrect log in credentials'
+    isPending.value = false
   }
 }
 
 const useLogIn = () => {
-  return { error, logIn }
+  return { error, logIn, isPending }
 }
 
 export default useLogIn
