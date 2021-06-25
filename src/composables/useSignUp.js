df7590c221ea4853f38a6bf692780c25ca47cf33
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-25 18:24:38 +0200

    Add pending state to useSignUp composable too

--- src/composables/useSignUp.js
+++ src/composables/useSignUp.js
@@ -2,9 +2,11 @@ import { ref } from "vue"
 import { projectAuth } from "../firebase/config"
 
 const error = ref(null)
+const isPending = ref(false)
 
 const signUp = async (email, password, displayName) => {
   error.value = null
+  isPending.value = true
 
   try {
     const res = await projectAuth.createUserWithEmailAndPassword(email, password)
@@ -15,17 +17,19 @@ const signUp = async (email, password, displayName) => {
 
     await res.user.updateProfile({ displayName })
     error.vale = null
+    isPending.value = false
     return res
 
   } catch(err) {
     console.log(err.message)
     error.value = err.message
+    isPending.value = false
   }
 
 }
 
 const useSignUp = () => {
-  return { error, signUp }
+  return { error, signUp, isPending }
 }
 
 export default useSignUp
