7b772f0df3850219e24673fa6d3f92811518bacd
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-26 01:00:40 +0200

    Add some styles and logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="navbar">
     <nav>
+      <img src="@/assets/logo.png" alt="Logo">
       <h1><router-link :to="{ name: 'Home' }">Musinity</router-link></h1>
+      <div class="links">
+        <button>Log Out</button>
+        <router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
+        <router-link class="btn" :to="{ name: 'Login' }">Log In</router-link>
+      </div>
     </nav>
-    <div class="links">
-      <button>Log Out</button>
-      <router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
-      <router-link class="btn" :to="{ name: 'Login' }">Log In</router-link>
-    </div>
   </div>
 </template>
 
@@ -17,6 +18,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a, button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
 </style>
