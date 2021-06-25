2588a45b3e15f6c55fb3033eadcf086aa1bb6c09
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-25 18:23:00 +0200

    Add pending state to useLogOut composable too

--- src/composables/useLogOut.js
+++ src/composables/useLogOut.js
@@ -2,20 +2,25 @@ import { ref } from "vue"
 import { projectAuth } from "../firebase/config"
 
 const error = ref(null)
+const isPending = ref(false)
 
 const logOut = async () => {
   error.value = null
+  isPending.value = true
+
   try {
     await projectAuth.signOut()
+    isPending = false
     
   } catch(err) {
     console.log(err.message)
     error.value = err.message
+    isPending = false
   }
 }
 
 const useLogOut = () => {
-  return { logOut, error }
+  return { logOut, error, isPending }
 }
 
 export default useLogOut
