8cb7a448f95466059f6e56303f682fc2f585dffc
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-26 00:42:36 +0200

    Add links to login and sign up components

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,12 +3,17 @@
     <nav>
       <h1><router-link :to="{ name: 'Home' }">Musinity</router-link></h1>
     </nav>
+    <div class="links">
+      <button>Log Out</button>
+      <router-link class="btn" :to="{ name: 'Signup' }">Sign Up</router-link>
+      <router-link class="btn" :to="{ name: 'Login' }">Log In</router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+
 }
 </script>
 
