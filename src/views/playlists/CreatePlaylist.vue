1508443ea1e775d1d00b82cbd41c4242405b2510
Pau Garcia Martínez <etpau@hotmail.es>
2021-06-29 00:06:56 +0200

    Set create playlist form

--- src/views/playlists/CreatePlaylist.vue
+++ src/views/playlists/CreatePlaylist.vue
@@ -1,7 +1,14 @@
 <template>
   
-  <p>Create playlist</p>
-  
+  <form>
+    <h4>Create New Playlist</h4>
+    <input type="text" placeholder="Playlist title" v-model="title" required>
+    <textarea placeholder="Playlist description..." v-model="description" required></textarea>
+    <label>Upload playlist cover image</label>
+    <input type="file">
+    <button>Create</button>
+  </form>
+
 </template>
 
 <script>
