7be587ac4e998f1700d5c1c58104790064ed21a0
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-04 23:12:33 +0200

    Add some structure to the list component

--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,5 +1,13 @@
 <template>
   
+  <div v-for="playlist in playlists" :key="playlist.id">
+    <div class="single">
+      <div class="thumbnail">
+        <img :src="playlist.coverUrl" alt="Playlist image">
+      </div>
+    </div>
+  </div>
+  
 </template>
 
 <script>
