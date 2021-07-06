ddc5b41adc0d52e1af14a7e1083c841a06760273
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-06 23:09:31 +0200

    Add some styles

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,17 +1,23 @@
 <template>
   
   <div v-if="error" class="error">{{ error }}</div>
-  <div v-if="document" class="playlist-details">
+  <div v-if="playlist" class="playlist-details">
+
     <div class="playlist-info">
       <div class="cover">
-        <img :src="document.coverUrl" alt="Cover image">
+        <img :src="playlist.coverUrl" alt="Cover image">
       </div>
-      <h2>{{ document.title }}</h2>
-      <p class="username">Created by {{ document.userName }}</p>
-      <p class="description">{{ document.description }}</p>
+      <h2>{{ playlist.title }}</h2>
+      <p class="username">Created by {{ playlist.userName }}</p>
+      <p class="description">{{ playlist.description }}</p>
     </div>
+
+    <div class="song-list">
+
+    </div>
+
   </div>
-  
+
 </template>
 
 <script>
@@ -20,13 +26,50 @@ import getDocument from '@/composables/getDocument'
 export default {
   props: ['id'],
   setup(props) {
-    const { document, error } = getDocument('playlists', props.id)
+    const { document: playlist, error } = getDocument('playlists', props.id)
 
-    return { document, error }
+    return { playlist, error }
   }
 }
 </script>
 
 <style>
-
+.playlist-details {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 80px;
+}
+.cover {
+  overflow: hidden;
+  border-radius: 20px;
+  position: relative;
+  padding: 160px;
+}
+.cover img {
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  min-width: 100%;
+  min-height: 100%;
+  max-width: 200%;
+  max-height: 200%;
+}
+.playlist-info {
+  text-align: center;
+}
+.playlist-info h2 {
+  text-transform: capitalize;
+  font-size: 28px;
+  margin-top: 20px;
+}
+.playlist-info p {
+  margin-bottom: 20px;
+}
+.username {
+  color: #999;
+}
+.description {
+  text-align: left;
+}
 </style>
