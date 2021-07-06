25eea93f05dfa0f5bf75b522c32a0034e254364b
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-06 23:06:54 +0200

    Create playlist info template

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -1,6 +1,16 @@
 <template>
   
-  <h2>Playlist Details</h2>
+  <div v-if="error" class="error">{{ error }}</div>
+  <div v-if="document" class="playlist-details">
+    <div class="playlist-info">
+      <div class="cover">
+        <img :src="document.coverUrl" alt="Cover image">
+      </div>
+      <h2>{{ document.title }}</h2>
+      <p class="username">Created by {{ document.userName }}</p>
+      <p class="description">{{ document.description }}</p>
+    </div>
+  </div>
   
 </template>
 
