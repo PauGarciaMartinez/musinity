dffabe238a3a12c32df988d55b0b239db859c674
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-08 11:58:19 +0200

    Import updateDoc composable and use spread operator to update songlist data

--- src/components/AddSong.vue
+++ src/components/AddSong.vue
@@ -14,12 +14,15 @@
 
 <script>
 import { ref } from '@vue/reactivity'
+import useDocument from '@/composables/useDocument'
 
 export default {
-  setup() {
+  props: ['playlist'],
+  setup(props) {
     const title = ref()
     const artist = ref()
     const showForm = ref(false)
+    const { updateDoc } = useDocument('playlists', props.playlist.id)
 
     const handleSubmit = async () => {
       const newSong = {
@@ -27,6 +30,11 @@ export default {
         artist: artist.value,
         id: Math.floor(Math.random() * 1000000000)
       }
+      await updateDoc({
+        songs: [...props.playlist.songs, newSong]
+      })
+      title.value = ''
+      artist.value = ''
     }
 
     return { title, artist, showForm, handleSubmit }
