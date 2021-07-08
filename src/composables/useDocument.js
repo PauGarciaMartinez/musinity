bf9d72d86b34ca7159b2862fed0edf09bb64733a
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-08 11:49:34 +0200

    Add update method to change songlist database

--- src/composables/useDocument.js
+++ src/composables/useDocument.js
@@ -23,7 +23,23 @@ const useDocument = (collection, id) => {
     }
   }
 
-  return { error, isPending, deleteDoc }
+  const updateDoc = async (updates) => {
+    isPending.value = true
+    error.value = null
+    
+    try {
+      const res = await docRef.update(updates)
+      isPending.value = false
+      return res
+
+    } catch(err) {
+      console.log(err.message)
+      error.value = err.message
+      isPending.value = false
+    }
+  }
+
+  return { error, isPending, deleteDoc, updateDoc }
 }
 
 export default useDocument
