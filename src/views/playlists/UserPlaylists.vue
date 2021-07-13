ee3318c45a55677af0b2a5ebd21d34581b7c042e
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-13 17:37:09 +0200

    Create a query argument to get only current user playlists

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -7,8 +7,19 @@
 </template>
 
 <script>
-export default {
+import getCollection from '@/composables/getCollection'
+import getUser from '@/composables/getUser'
 
+export default {
+  setup() {
+    const { user } = getUser()
+    const { document: playlists } = getCollection(
+      'playlists', 
+      ['userId', '==', user.value.uid]
+    )
+
+    return { playlists }
+  }
 }
 </script>
 
