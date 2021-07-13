65d71fda452375102bb1e9f99858cfdad188b0f9
Pau Garcia Martínez <etpau@hotmail.es>
2021-07-13 17:46:30 +0200

    List current user playlists

--- src/views/playlists/UserPlaylists.vue
+++ src/views/playlists/UserPlaylists.vue
@@ -2,6 +2,10 @@
   
   <div class="user-playlist">
     <h2>My Playlists</h2>
+    <div v-if="playlists">
+      <ListView :playlists="playlists" /> 
+    </div>
+    <router-link :to="{ name: 'CreatePlaylist' }" class="btn">Create a New Playlist</router-link>
   </div>
   
 </template>
@@ -9,11 +13,15 @@
 <script>
 import getCollection from '@/composables/getCollection'
 import getUser from '@/composables/getUser'
+import ListView from '@/components/ListView'
 
 export default {
+  components: {
+    ListView
+  },
   setup() {
     const { user } = getUser()
-    const { document: playlists } = getCollection(
+    const { documents: playlists } = getCollection(
       'playlists', 
       ['userId', '==', user.value.uid]
     )
